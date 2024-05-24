bbed818b8f2b25ca89b1744aa8b0b21e51f6dee7
NanoCode10 <marianoars10@gmail.com>
2024-05-24 15:45:03 -0300

    Mi Proyecto Galeria Nft para Codo a Codo 1.0.4 👍

--- src/js/components/registrate.js
+++ src/js/components/registrate.js
@@ -1,10 +1,32 @@
 
+const errorMessage = document.querySelector("#error-message")
+
 document
   .getElementById("signupForm")
-  .addEventListener("submit", function (event) {
+  .addEventListener("submit", (event) => {
     event.preventDefault();
-    // Aquí puedes agregar tu lógica para registrar un nuevo usuario
-    // y realizar las acciones necesarias
-    console.log("Registrarse");
+
+
+    const form = event.target
+    const nombre = form.name.value
+    const apellido = form.apellido.value
+    const email = form.email.value
+    const password = form.password.value
+
+    if (nombre.length === "" || apellido === "" || email === "" || password.length < 8) {
+      errorMessage("Todos los campos son obligatorios y la password de 8 carcteres minimo");
+      return;
+    
+      return;
+    }
+
+
+    
+    console.log(nombre, apellido, email, password);
+
+
+
+
     alert("Exitos!");
   });
+ 
