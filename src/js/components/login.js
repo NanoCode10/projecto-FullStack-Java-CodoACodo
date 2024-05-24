f368d8e6d70dd4fd9df6a1fbfd91be9ba1a0fbad
NanoCode10 <marianoars10@gmail.com>
2024-05-24 14:27:41 -0300

    Mi Proyecto Galeria Nft para Codo a Codo 1.0.4 👍

--- src/js/components/login.js
+++ src/js/components/login.js
@@ -8,12 +8,3 @@ document
     alert("Exitos!");
   });
 
-document
-  .getElementById("signupForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    // Aquí puedes agregar tu lógica para registrar un nuevo usuario
-    // y realizar las acciones necesarias
-    console.log("Registrarse");
-    alert("Exitos!");
-  });
--- src/js/components/registrate.js
+++ src/js/components/registrate.js
@@ -1,12 +1,3 @@
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    // Aquí puedes agregar tu lógica para verificar las credenciales de inicio de sesión
-    // y realizar las acciones necesarias
-    console.log("Iniciar Sesión");
-    alert("Exitos!");
-  });
 
 document
   .getElementById("signupForm")
