850dfd1cef7eb9dc665388c5c5df0b2e938603d9
NanoCode10 <marianoars10@gmail.com>
2024-05-28 10:36:23 -0300

    Mi Proyecto Galeria Nft para Codo a Codo 1.0.5 👍

--- src/js/components/login.js
+++ src/js/components/login.js
@@ -2,9 +2,31 @@ document
   .getElementById("loginForm")
   .addEventListener("submit", function (event) {
     event.preventDefault();
-    // Aquí puedes agregar tu lógica para verificar las credenciales de inicio de sesión
-    // y realizar las acciones necesarias
-    console.log("Iniciar Sesión");
-    alert("Exitos!");
+   
+    const form = event.target
+    console.log(form)
+    const email = form.email.value;
+    const password = event.target.password.value
+    let user = {
+      email,
+      password,
+    };
+    
+    //verificar si el usuer y la password existen en localStorage
+    let users = JSON.parse(localStorage.getItem("users"));
+    
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].email === user.email && users[i].password === user.password) {
+        
+
+         //redireccionar al home
+        window.location.href = "./home.html";
+
+        return;
+      }}
+
+
+   //si el usuario no existe o la password no coincide
+    alert("No se encontro usuario registrado o la password no coicide!");
   });
 
--- src/js/components/registrate.js
+++ src/js/components/registrate.js
@@ -14,13 +14,35 @@ document
     const password = form.password.value
 
     if (nombre.length === "" || apellido === "" || email === "" || password.length < 8) {
-      errorMessage("Todos los campos son obligatorios y la password de 8 carcteres minimo");
+      errorMessage.textContent = "Todos los campos son obligatorios y la password de 8 carcteres minimo";
       return;
     
-      return;
+      
     }
 
-
+    //guardar el usuario en la base de datos (LocalStorage)
+    const user = {
+      nombre,
+      apellido,
+      email,
+      password,
+    };
+    
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    
+    const userExists = users.find((u) => u.email === email);
+    
+    if (userExists) {
+      errorMessage.textContent ="El email ya esta registrado";
+      return;
+    }
+    
+    users.push(user);
+    localStorage.setItem("users", JSON.stringify(users));
+    
+    //redireccionar al home
+    window.location.href = "./home.html";
+    
     
     console.log(nombre, apellido, email, password);
 
